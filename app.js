f238132cff935d5091d6b356b7dcb81ba73c96e7
Esdras <esdrastmendes@gmail.com>
2022-06-02 19:22:08 -0300

    feat: middlewares de products e sales

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const products = require('./controllers/controllerProducts');
 const sales = require('./controllers/controllerSales');
+const salesMiddleware = require('./middlewares/salesMiddlewares');
+const productsMiddleware = require('./middlewares/productsMiddleware');
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -11,9 +13,14 @@ app.get('/', (_request, response) => {
 
 app.get('/products', products.findProducts);
 app.get('/products/:id', products.findProductById);
-
+app.post('/products', productsMiddleware, (_req, _res) => {
+  console.log('TESTE');
+});
 app.get('/sales', sales.findSales);
 app.get('/sales/:id', sales.findSaleById);
+app.post('/sales', salesMiddleware, (_req, _res) => {
+  console.log('TESTE sales');
+});
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
